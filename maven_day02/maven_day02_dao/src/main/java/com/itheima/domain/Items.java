eed57582fdf2f33319eb815d27f3a5b5f6fcea7e
admin <admin@itheima.com.cn>
2018-12-21 18:36:01 +0800

    gsdf

---
+++ maven_day02/maven_day02_dao/src/main/java/com/itheima/domain/Items.java
@@ -0,0 +1,78 @@
+package com.itheima.domain;
+
+import java.util.Date;
+
+/**
+ * @program: maven_day02
+ * @description: TODO
+ * @author: Miss.Chen
+ * @create 2018-12-20 21:30
+ **/
+public class Items {
+    private Integer id;
+    private String name;
+    private Float price;
+    private String pic;
+    private Date createtime;
+    private String detail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", pic='" + pic + '\'' +
+                ", createtime=" + createtime +
+                ", detail='" + detail + '\'' +
+                '}';
+    }
+}
